70bcf3e6d7b4dd3e3006405a6ef19658815f3673
Benjamin M <benja.tush@gmail.com>
2019-11-29 00:35:23 +0300

    feat(design): added logo and favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,33 +1,6 @@
 export default {
   mode: 'universal',
   /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-      // TODO: find how to do this better
-      {
-        hid: 'author',
-        name: 'keywords',
-        content: 'investment, investments, ziel, ziel fresh, jaza tanki',
-      },
-      {
-        hid: 'author',
-        name: 'author',
-        content: 'Upscale Technologies',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-  /*
    ** Customize the progress-bar color
    */
   loading: { color: '#fff' },
@@ -79,5 +52,111 @@ export default {
     extend(config, ctx) {},
     vendor: [],
     plugins: [],
+  },
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+      // TODO: find how to do this better
+      {
+        hid: 'author',
+        name: 'keywords',
+        content: 'investment, investments, ziel, ziel fresh, jaza tanki',
+      },
+      {
+        hid: 'author',
+        name: 'author',
+        content: 'Upscale Technologies',
+      },
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '57x57',
+        href: '/favicon/apple-icon-57x57.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '60x60',
+        href: '/favicon/apple-icon-60x60.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '72x72',
+        href: '/favicon/apple-icon-72x72.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '76x76',
+        href: '/favicon/apple-icon-76x76.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '114x114',
+        href: '/favicon/apple-icon-114x114.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '120x120',
+        href: '/favicon/apple-icon-120x120.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '144x144',
+        href: '/favicon/apple-icon-144x144.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '152x152',
+        href: '/favicon/apple-icon-152x152.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-icon-180x180.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192"',
+        href: '/favicon/android-icon-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicon/favicon-96x96.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
+    ],
   },
 };
