67c55e2c1d09c2c7e0bb217e8c65a82145e7e5cb
Benjamin M <benja.tush@gmail.com>
2019-12-02 12:36:13 +0300

    feat(products): finished first field power version

--- pages/products/field-power.vue
+++ pages/products/field-power.vue
@@ -1,72 +1,238 @@
 <template lang="pug">
   div
     SubHeader(title="Mobile Field Service Management Software" titleMenu="Field Power")
-    // Start Page
+    // Start About Us
     section.section
       .container
         .row
           .col-lg-12
-            .mt-3
-              img.img-fluid.mx-auto.d-block.rounded(src="~/assets/images/single_work/work_detail.jpg" alt="")
-        .row.mt-3.vertical-content
-          .col-lg-8.mt-3
-            .work-detail-title
-              h1.mb-1 Modern,Clean & Minimal Design
-              h6.text-muted.font-weight-normal Branding, Product Design and Marketing
-            .mt-4.work_detail_desc
-              p.text-muted
-                | There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum.
-              p.text-muted
-                | It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-          .col-lg-4.mt-3
-            .work_detail_list
-              div
-                h6.text-custom.mb-1 Client:
-                p Axzisz Themes
-              div
-                h6.text-custom.mb-1 Type:
-                p UI Elements, Icons
-              div
-                h6.text-custom.mb-1 Date:
-                p 28 January, 2019
-              .mt-4
-                ul.list-unstyled.list-inline.mb-0.work_single_icon
-                  li.list-inline-item
-                    a(href="javascript:void(0);")
-                      i.mdi.mdi-facebook-box
-                  li.list-inline-item
-                    a(href="javascript:void(0);")
-                      i.mdi.mdi-twitter
-                  li.list-inline-item
-                    a(href="javascript:void(0);")
-                      i.mdi.mdi-dribbble
-                  li.list-inline-item
-                    a(href="javascript:void(0);")
-                      i.mdi.mdi-instagram
-                  li.list-inline-item
-                    a(href="javascript:void(0);")
-                      i.mdi.mdi-pinterest
-    // End Page
-    // Start Cta
+            .section_title.text-center
+              p.small_title.mb-4.text-custom.text-uppercase  FieldPower
+              h2.text-capitalize.mx-auto.section_header Award Winning Mobile Field Service Management Software & solution
+              p.sec_content.pt-3.text-muted.mx-auto
+                | Customers demand your company to deliver on-time services that they pay for, and your company relies on its back-end office staff and technicians to meet these expectations. That means it’s important that field technicians maintain their assigned schedules, even while away from the office. Also, it’s mandatory for the management to know the mobile field service team’s real-time location, their performance, and their adherence with assigned jobs and hours.
+    // End About Us
+    // Start How It Work
+    section.section.bg-light
+      .container
+        .row
+          .col-lg-12
+            .section_title.text-center
+              p.small_title.mb-4.text-custom.text-uppercase Solutions Offered
+              h2.text-capitalize.mx-auto.section_header Our Features
+              p.sec_content.pt-3.text-muted.mx-auto
+                | Rapidly deployable, immensely scalable and great future growth promises with every service.
+        .row.mt-3
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Intelligent Scheduling
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon6.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Dynamic Dispatch
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon71.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Travel/GPS Optimization
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon8.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Multi-Vendor Management
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon9.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Back Office Integration
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon10.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Accounts, Metrics and Reports
+        .row.mt-3
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Forecasting and Planning
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Mobile Field Service
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Parts and Inventory Management
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Contract and Warranty Management
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Customer Self Service Portal
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Depot Repair
+        .row.mt-3
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Service Project Management
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Knowledge Management
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Business Intelligence
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Asset Management
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Order Processing- Sales and Service
+          .col-lg-2.col-6
+            .text-center.position-relative.hit_box.rounded.mt-3.py-3
+              .hit_icon.text-custom
+                img.icon-box-image(src="~/assets/images/fieldPower/features_icon5.jpg")
+              .hit_content.mt-2
+                h6.font-weight-bold Integration Kit’ to ERPs/CRMs
+    // End How It Work
+    // Start Faq
     section.section
       .container
         .row
           .col-lg-12
-            .cta_sec.text-center.text-white.bg-gradient
-              h2.font-weight-bold Are You Ready To Enjoy? Get This Perfect Theme Now!
-              p.mx-auto.cta-desc.mb-0.mt-4.pt-2
-                | It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-              .mt-4.pt-3.position-relative.z-index
-                a.btn.btn-white.btn-rounded(href="#") Start Trial Free
-    // End Cta
+            .section_title.text-center
+              p.small_title.mb-4.text-custom.text-uppercase Why FieldPower?
+              h2.text-capitalize.mx-auto.section_header Benefits
+              p.sec_subtitle.pt-3.text-muted.mx-auto
+                | Our field service software offers enormous benefits for:
+        .row.mt-5.vertical-content
+          .col-lg-5
+            .faq_img.mt-3
+              img.img-fluid.mx-auto.d-block(src="~/assets/images/faq.png" alt="")
+          .col-lg-7
+            .faq_main.mt-3
+              #accordionExample.accordion
+                .card
+                  #headingOne.card-header
+                    h5.mb-0.align-middle(data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne")
+                      i.mdi.mdi-lightbulb-outline.h4
+                      span To Technicians:
+                  #collapseOne.collapse.show(aria-labelledby="headingOne" data-parent="#accordionExample")
+                    .card-body
+                      ul
+                        li
+                          | With a site’s service history at a tech’s fingertips, he can quickly learn supportive tips like what parts have caused trouble previously and what he needs to look out for.
+                        li
+                          | The comprehensive inventory managing feature in field service management software will ensure that he’s never stuck at a site without the part he needs. Thus helping to improve first time fix rates.
+                        li
+                          | Replacing manual task of excel sheet updates, techs can digitally record about the job, including work descriptions, labor time, used materials, images and a customer signature before leaving their job-site. Check list will ensure mobile field service team has completed the assigned job without missing anything.
+                        li
+                          | Besides, he has less down time in his day because he never has to go back to the office to turn in invoices or pick up work orders.
+                        li
+                          | Finally, with a Google or Microsoft integrated GPS solution, a technician will be led to a job through the optimized route possible.
+                .card
+                  #headingTwo.card-header
+                    h5.collapsed.mb-0(data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo")
+                      i.mdi.mdi-lightbulb-outline.h4
+                      span To Field Managers and Dispatchers:
+                  #collapseTwo.collapse(aria-labelledby="headingTwo" data-parent="#accordionExample")
+                    .card-body
+                      ul
+                        li
+                          | With a field service software, Jobs are automatically assigned to the field technicians on the basis of location, time, skills or any particular brief given by the customer keeping up to the companies standard and 100 % customer satisfaction.
+                        li
+                          | Oversee the precise status of technicians presently working in the field on various requests.
+                        li
+                          | Automatic schedule alerts for workers give you peace of mind knowing your employees won’t miss another appointment.
+                        li
+                          | Will reflect apparent trends in technician productivity versus schedule saturation to best prepare for busy episodes and helps manager to weed out technicians who aren’t meeting the enterprise’s standard.
+                        li
+                          | Saves time by making task fully automated that is difficult to do on paper manually.
+                .card
+                  #headingThree.card-header
+                    h5.collapsed.mb-0(data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree")
+                      i.mdi.mdi-lightbulb-outline.h4
+                      span To CXO:
+                  #collapseThree.collapse(aria-labelledby="headingThree" data-parent="#accordionExample")
+                    .card-body
+                      ul
+                        li
+                          | Workforce management software scales up the business without the need for additional back office personnel.
+                        li
+                          | With the accessibility of the data on a central platform, better co-ordination can be achieved.
+                        li
+                          | Routings and driving times will be optimized, reducing mileage and fuel costs, increasing the number of work orders a day for your techs resulting in increased business and profits.
+                        li
+                          | Real-time access to the visibility of your costs and effective business reporting helps to calibrate the business mile precisely.
+                        li
+                          | Provides validation of the operational output with reference to the job status, the actual time of arrival, time taken to complete a task etc.
+                .card
+                  #headingzzzzfpor.card-header
+                    h5.collapsed.mb-0(data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour")
+                      i.mdi.mdi-lightbulb-outline.h4
+                      span To Customers:
+                  #collapseFour.collapse(aria-labelledby="headingzzzzfpor" data-parent="#accordionExample")
+                    .card-body
+                      ul.edx(style="padding-left:30px;")
+                        li
+                          | Customers can enter issue tickets by logging onto the application and can access their past repair history and relevant support information.
+                        li
+                          | Once the job is accepted by a field technician. Customer will be notified with technician’s details and his arrival.
+                        li
+                          | The customer receives a complete description of the job order executed, including the technician detail, service done and it’s pricing.
+                        li Customer can pay for the job if it was completed satisfactorily. 
+                        li
+                          | Through this workforce management software’s customer portal, they can schedule appointments online at any time without having to call the office everytime.
+    // End Faq
+    Cta
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import SubHeader from '~/components/SubHeader.vue';
+import Cta from '~/components/Cta.vue';
 
 @Component({
-  components: { SubHeader },
+  components: { SubHeader, Cta },
 })
 export default class FieldPower extends Vue {}
 </script>
