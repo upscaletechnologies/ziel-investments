efca9c286cfded97706ceb1bbce8fb6c4db88d42
Benjamin M <benja.tush@gmail.com>
2020-06-24 15:06:20 +0300

    feat(fancybox): added a better fancybox image layout

--- pages/products/ziel-fresh.vue
+++ pages/products/ziel-fresh.vue
@@ -4,6 +4,53 @@
     // Start Page
     section.section
       .container
+        .row.mb-4
+          .col-6.col-lg-4
+            figure
+              a.d-block.mb-4(data-fancybox="images" href="https://source.unsplash.com/lSXpV8bDeMA/1536x2304" data-width="1536" data-height="2304")
+                img.img-fluid(src="https://source.unsplash.com/lSXpV8bDeMA/416x623")
+              figcaption
+                h6 Rocky mountain under blue and white sky
+                a(href="https://unsplash.com/photos/lSXpV8bDeMA") Photo by Guillaume Briard
+            figure
+              a.d-block.mb-4(data-fancybox="images" href="https://source.unsplash.com/ty4X72BSsXY/1279x853" data-width="1279" data-height="853")
+                img.img-fluid(src="https://source.unsplash.com/ty4X72BSsXY/416x278")
+              figcaption
+                h6 Vaihingen an der Enz, Germany
+                a(href="https://unsplash.com/photos/ty4X72BSsXY") Photo by Oliver Roos
+          .col-6.col-lg-4
+            figure
+              a.d-block.mb-4(data-fancybox="images" href="https://source.unsplash.com/QkPb5g9p338/1279x719" data-width="1279" data-height="719")
+                img.img-fluid(src="https://source.unsplash.com/QkPb5g9p338/416x234")
+              figcaption
+                h6 Closeup photo of world globe
+                a(href="https://unsplash.com/photos/QkPb5g9p338") Photo by chuttersnap
+            figure
+              a.d-block.mb-4(data-fancybox="images" href="https://source.unsplash.com/z55CR_d0ayg/1279x853" data-width="1279" data-height="853")
+                img.img-fluid(src="https://source.unsplash.com/z55CR_d0ayg/416x278")
+              figcaption
+                h6 Blessed are the curious, for they shall have adventures. 🚩
+                a(href="https://unsplash.com/photos/z55CR_d0ayg") Photo by Andrew Neel
+            figure
+              a.d-block.mb-4(data-fancybox="images" href="https://source.unsplash.com/IbLZjKcelpM/1020x858" data-width="1020" data-height="858")
+                img.img-fluid(src="https://source.unsplash.com/IbLZjKcelpM/416x350")
+              figcaption
+                h6 Man holding pair of ski poles in front of trees
+                a(href="https://unsplash.com/photos/IbLZjKcelpM") Photo by Oziel Gómez
+          .col-6.col-lg-4.d-none.d-lg-block
+            figure
+              a.d-block.mb-4(data-fancybox="images" href="https://source.unsplash.com/KgCbvOWYuU0/1279x870" data-width="1279" data-height="870")
+                img.img-fluid(src="https://source.unsplash.com/KgCbvOWYuU0/416x283")
+              figcaption
+                h6 Aerial photo of person using paddleboard
+                a(href="https://unsplash.com/photos/KgCbvOWYuU0") Photo by Ishan @seefromthesky
+            figure
+              a.d-block.mb-4(data-fancybox="images" href="https://source.unsplash.com/O7qK1vQY3p0/1519x2279" data-width="1519" data-height="2279")
+                img.img-fluid(src="https://source.unsplash.com/O7qK1vQY3p0/416x623")
+              figcaption
+                h6 Car on curve road surrounded by trees
+                a(href="https://unsplash.com/photos/O7qK1vQY3p0") Photo by Grant Porter
+
         .row
           .col-lg-12
             .section_title.text-center
