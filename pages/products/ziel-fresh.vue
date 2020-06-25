d8ffa8292c7c560e896387070d303ed562d29322
Joseph Mbuku <jowey-jose@users.noreply.github.com>
2020-06-25 07:21:57 +0300

    fix(ziel-fresh): updated the gallery with correct videos

--- pages/products/ziel-fresh.vue
+++ pages/products/ziel-fresh.vue
@@ -9,14 +9,16 @@
             .section_title.text-center
               p.small_title.mb-4.text-custom.text-uppercase Ziel Fresh
               h2.text-capitalize.mx-auto.section_header Fresh Bottled Water
+              p.sec_content.pt-3.text-muted.mx-auto.
+                You can order the water in these packagings: #[strong 500ml and 1 litre] dozen package, #[strong 18.9 litres] Refillable or Disposable.
         .row.mb-4
           .col-6.col-lg-4
             figure
               a.d-block.mb-4(data-fancybox="images" href="image-1.jpeg" data-width="1536" data-height="2304")
                 img.img-fluid(src="~/assets/images/products/image-1-1.jpeg")
             figure
-              a.d-block.mb-4(data-fancybox="images" href="/image-2.png" data-width="1279" data-height="853")
-                img.img-fluid(src="~/assets/images/products/image-2-1.png")
+              a.d-block.mb-4(data-fancybox="images" href="/image-2.jpeg" data-width="1279" data-height="853")
+                img.img-fluid(src="~/assets/images/products/image-2-1.jpeg")
           .col-6.col-lg-4
             figure
               a.d-block.mb-4(data-fancybox="images" href="image-3.jpeg" data-width="1279" data-height="719")
@@ -34,36 +36,56 @@
             figure
               a.d-block.mb-4(data-fancybox="images" href="image-7-1.jpeg" data-width="1519" data-height="2279")
                 img.img-fluid(src="~/assets/images/products/image-7-1.jpeg")
+        .row
+          .col-lg-12
+            .section_title.text-center
+              h2.text-capitalize.mx-auto.section_header Our Story:
         .card-deck.mb-sm-4.video-deck
-          .card
-            a(data-fancybox="bigbuckbunny" data-width="640" data-height="360" href="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4")
-              img.card-img-top.img-fluid(src="~/assets/images/products/poster-1.jpeg")
-            .card-body
-              p.card-text
-                | About Ziel Fresh Company Tour
           .card
             a(data-fancybox="html5-video" href="#myVideo")
               img.card-img-top.img-fluid(src="~/assets/images/products/poster-1.jpeg")
             .card-body
-              p.card-text Ziel Fresh Work Environment
+              p.card-text Factory tour: 1
             video#myVideo(width="640" height="320" controls="" style="display:none;")
-              source(src="https://www.html5rocks.com/en/tutorials/video/basics/Chrome_ImF.mp4" type="video/mp4")
-              source(src="https://www.html5rocks.com/en/tutorials/video/basics/Chrome_ImF.webm" type="video/webm")
-              source(src="https://www.html5rocks.com/en/tutorials/video/basics/Chrome_ImF.ogv" type="video/ogg")
-              |             Your browser doesn't support HTML5 video tag.
+              source(src="~/assets/videos/video-1.mp4" type="video/mp4")
+              | Your browser doesn't support HTML5 video tag.
+
+          .card
+            a(data-fancybox="html5-video" href="#myVideo")
+              img.card-img-top.img-fluid(src="~/assets/images/products/poster-2.jpeg")
+            .card-body
+              p.card-text Factory tour: 2
+            video#myVideo(width="640" height="320" controls="" style="display:none;")
+              source(src="~/assets/videos/video-2.mp4" type="video/mp4")
+              | Your browser doesn't support HTML5 video tag.
+          .card
+            a(data-fancybox="html5-video" href="#myVideo")
+              img.card-img-top.img-fluid(src="~/assets/images/products/poster-3.jpeg")
+            .card-body
+              p.card-text Factory tour: 3
+            video#myVideo(width="640" height="320" controls="" style="display:none;")
+              source(src="~/assets/videos/video-5.mp4" type="video/mp4")
+              | Your browser doesn't support HTML5 video tag.
         .row
           .col-lg-12
             .section_title.text-center
-              h2.text-capitalize.mx-auto.section_header About Us
-              p.sec_content.pt-3.text-muted.mx-auto.
-                #[strong Ziel] translated to english means a goal, aim, objective, target, destination or finishing line.
+              h2.text-capitalize.mx-auto.section_header About Ziel Fresh
+            .section_title.text-left
+              p.
+                #[strong Ziel] translated to english means a goal, aim, objective, target, destination or finishing line. 
+              p.
                 #[strong Fresh] simply means something recently harvested.
-                #[strong Ziel Fresh] purified water is bottled fresh at the source to maintain its natural taste which is balanced by nature. Its enhanced by use of advanced modern technology by use of multi-barrier filtration, reverse osmosis, ozanation and UV treatment to give it a refreshing natural taste.
-                #[br]
+              p.
+                #[strong Ziel Fresh] purified water is bottled fresh at the source to maintain its natural taste which is balanced by nature. 
+              p.
+               Its enhanced by use of advanced modern technology by use of multi-barrier filtration, reverse osmosis, ozanation and UV treatment to give it a refreshing natural taste.
+              p.
                 Our water quality, grade and above all palate features are 100% natural
+        .row
+          .col-lg-12
+            .section_title.text-center
               p.sec_content.pt-3.text-muted.mx-auto.
-                You can order the water in these packagings: #[strong 500ml and 1 litre] dozen package, #[strong 18.9 litres] Refillable or Disposable.
-                For enquiries, please call #[a.text-custom.nuxt-link-exact-active.nuxt-link-active(href='tel: +254 713-917-108') +254 713-917-108] or #[a.text-custom.nuxt-link-exact-active.nuxt-link-active(href='tel: +254 727-238-558') +254 727-238-558]
+                For enquiries, please call us on: #[a.text-custom.nuxt-link-exact-active.nuxt-link-active(href='tel: +254 713-917-108') +254 713-917-108] or #[a.text-custom.nuxt-link-exact-active.nuxt-link-active(href='tel: +254 727-238-558') +254 727-238-558]
 
     // Start How It Work
     // Start Description
@@ -100,7 +122,7 @@
                   p.features-sub-title.pt-2
                     | Ziel Fresh  premium is manufactured in the state-of-the-art, ultra modern and computerised equipment with stringent quality control measures.
       .features_side_mockup.d-lg-block.d-xl-block.d-none.right
-        img.img-fluid.mx-auto.d-block(src="~/assets/images/products/ziel-fresh.jpeg" alt="")
+        img.img-fluid.mx-auto.d-block(src="~/assets/images/products/ziel-fresh-10.jpeg" alt="")
     // End Description
     // Start Page
     section.section.bg-light
